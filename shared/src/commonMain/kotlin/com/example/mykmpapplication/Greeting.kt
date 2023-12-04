ff167de0a2b390f8606e98b71d9ad76f1f107da0
koko57 <aa@MacBook-Pro.local>
2023-12-04 19:55:31 +0100

    feat: add a multiplatform dependency

--- shared/src/commonMain/kotlin/com/example/mykmpapplication/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/mykmpapplication/Greeting.kt
@@ -5,8 +5,11 @@ import kotlin.random.Random
 class Greeting {
     private val platform: Platform = getPlatform()
 
+    val daysPhrase = "There are only ${daysUntilNewYear()} days left until New Year! 🎆"
+
     fun greet(): List<String> = buildList {
         add(if (Random.nextBoolean()) "Hi!" else "Hello!")
         add("Guess what it is! > ${platform.name.reversed()}!")
+        add(daysPhrase)
     }
 }
